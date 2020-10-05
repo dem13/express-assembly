98f82da966af68029224d0ef43f9fc36dc2f3619
demyansson <idcadifi@gmail.com>
2020-10-05 20:11:07 +0300

    feat(app): secure app

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const express = require('express');
+const helmet = require('helmet');
+const morgan = require('morgan');
+
 const app = express();
 
 const port = process.env.PORT || 3000;
+
+app.use(helmet());
+app.use(morgan('combined'));
 
 app.get('/', (req, res) => res.send('Hello, world!'));
 
