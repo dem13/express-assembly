fb7bffbef4e75b4606a11aadd83a94d762cee955
demyansson <idcadifi@gmail.com>
2020-10-05 20:19:15 +0300

    feat(app): request body parser

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const helmet = require('helmet');
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 const app = express();
 
@@ -8,8 +9,15 @@ const port = process.env.PORT || 3000;
 
 app.use(helmet());
 app.use(morgan('combined'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
-app.get('/', (req, res) => res.send('Hello, world!'));
+app.get('/', (req, res) => {
+    res.send({
+        message: 'Hello, world!',
+        data: req.body,
+    });
+});
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
