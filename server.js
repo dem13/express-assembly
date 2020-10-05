283649711a8eff1ccd24dd2b7e50b30297cdc160
demyansson <idcadifi@gmail.com>
2020-10-05 20:20:16 +0300

    feat(app): allow cors

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const helmet = require('helmet');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
 
@@ -11,6 +12,7 @@ app.use(helmet());
 app.use(morgan('combined'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send({
