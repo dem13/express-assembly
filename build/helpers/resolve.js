c3b2d5fce490ef22c5142f66adb7e8ccf192fa33
demyansson <idcadifi@gmail.com>
2020-10-12 22:52:14 +0300

    feat(app): delete build

--- build/helpers/resolve.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const tsyringe_1 = require("tsyringe");
-exports.default = (token) => {
-    return tsyringe_1.container.resolve(token);
-};
--- build/src/loaders/configLoader.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const node_config_ts_1 = require("node-config-ts");
-exports.default = (options) => {
-    console.log(node_config_ts_1.config);
-};
